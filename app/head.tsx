27cec055058a1fc6e958bcc7aa704fef4a066967
zmr7 <zmr7@calvin.edu>
2025-05-28 20:26:31 -0400

    Added metadat and fixed the hero

--- app/head.tsx
+++ app/head.tsx
@@ -1,25 +1,16 @@
-export default function Head() {
-  return (
-    <>
-      <title>Keystone</title>
-      <meta name="description" content="Waitlist Keystone now." />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
-      
-      {/* Open Graph / Social Media Meta Tags */}
-      <meta property="og:title" content="Keystone" />
-      <meta property="og:description" content="Waitlist Keystone now." />
-      <meta property="og:type" content="website" />
-      <meta property="og:image" content="https://thekeystoneapp.com/og-image.png" />
-      <meta property="og:url" content="https://thekeystoneapp.com" />
-      {/*<meta property="og:image" content="/og-image.png" />*/}
-      
-      {/* Twitter Meta Tags */}
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content="Keystone" />
-      <meta name="twitter:image" content="https://thekeystoneapp.com/og-image.png" />
-      <meta name="twitter:description" content="Waitlist Keystone now." />
-      {/*<meta name="twitter:image" content="/og-image.png" />*/}
-    </>
-  );
-} 
+export const Metadata = {
+  title: "Keystone",
+  description: "Waitlist Keystone now.",
+  openGraph: {
+    title: "Keystone",
+    description: "Waitlist Keystone now.",
+    url: "https://thekeystoneapp.com",
+    images: ["https://thekeystoneapp.com/og-image.png"],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Keystone",
+    description: "Waitlist Keystone now.",
+    images: ["https://thekeystoneapp.com/og-image.png"],
+  },
+};
