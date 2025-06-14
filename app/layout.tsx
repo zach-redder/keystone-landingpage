2c7a3cdf6d86f6c6ae084dde719cd1d0ea4c9fda
zach-redder <zmr7@calvin.edu>
2025-06-14 11:07:28 -0400

    why

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,23 +6,23 @@ import { Analytics } from "@vercel/analytics/next"
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "Keystone",
-  description: "Waitlist Keystone now.",
+  title: "Waitlist Keystone now",
+  description: "Waitlist Keystone now",
   icons: {
     icon: '/icon.png',
     shortcut: '/icon.png',
     apple: '/icon.png',
   },
   openGraph: {
-    title: "Keystone",
-    description: "Waitlist Keystone now.",
+    title: "Waitlist Keystone now",
+    description: "Waitlist Keystone now",
     url: "https://thekeystoneapp.com",
     images: ["https://thekeystoneapp.com/og-image.png"],
   },
   twitter: {
     card: "summary_large_image",
-    title: "Keystone",
-    description: "Waitlist Keystone now.",
+    title: "Waitlist Keystone now",
+    description: "Waitlist Keystone now",
     images: ["https://thekeystoneapp.com/og-image.png"],
   },
 };
